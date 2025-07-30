261b863763b63dadf0153036e18893fa5b3224bd
bazmo36 <mnalqallaf362@gmail.com>
2025-07-30 16:43:29 +0300

    Change form to override methode

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -7,6 +7,7 @@ const multer = require("multer")
 const upload = multer({ dest: 'uploads/' })
 
 
+
 // Profile Page
 router.get("/profile",isSignedIn,async(req,res)=>{
     try{
@@ -31,23 +32,49 @@ router.get("/edit-profile",isSignedIn, async(req,res)=>{
 })
 
 
-router.post("/edit-profile", isSignedIn, upload.single("profilePic"), async (req, res) => {
+router.put("/edit-profile",
+isSignedIn, upload.single("profilePic"),async (req, res) => {
     try {
-        const updateData = { bio: req.body.bio || "" };
+      const updateData = { bio: req.body.bio || "" };
 
-        if (req.file) {
-            updateData.profilePic = "/uploads/" + req.file.filename; // Save path in DB
-        }
+      if (req.file) {
+        updateData.profilePic = "/uploads/" + req.file.filename; // Save path in DB
+      }
 
-        const updatedUser = await User.findByIdAndUpdate(req.session.user._id, updateData, { new: true });
-        req.session.user = updatedUser;
+      const updatedUser = await User.findByIdAndUpdate(
+        req.session.user._id,
+        updateData,
+        { new: true }
+      );
+      req.session.user = updatedUser;
 
-        res.redirect("/users/profile");
-    } catch (error) {
-        console.log("Error updating profile:", error);
-        res.render("users/edit-profile.ejs", { user: req.session.user, error: "Update failed" });
+      res.redirect("/users/profile");
+    } 
+     catch (error) {
+      console.log("Error updating profile:", error);
+      res.render("users/edit-profile.ejs", {
+        user: req.session.user,
+        error: "Update failed",
+      });
     }
+  }
+);
+
+
+//Delete Profile
+router.delete("/delete", isSignedIn, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.session.user._id);
+    req.session.destroy(() => {
+      res.redirect("/auth/sign-up");
+    });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.redirect("/users/profile");
+  }
 });
+
+
 
 
 module.exports = router
